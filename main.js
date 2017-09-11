7079f89de565bba95f3f57bf37cffbdad70a20da
Ellis Mwekunda <Scareb15@gmail.com>
2017-09-11 11:55:49 -0600

    added third API call

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 var el = document.getElementById('form')
+var key = 'AIzaSyDydF9_qyXVTdAs8G7IFUJngvSu1Ic7kSY'
 el.addEventListener('submit', (event) => {
     event.preventDefault();
     var id = event.target.elements.address_input.value;
-    console.log(id)
-    var key = 'AIzaSyDydF9_qyXVTdAs8G7IFUJngvSu1Ic7kSY'
+    //console.log(id)
+
     fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${id}&key=${key}`)
         .then((response) => {
             return response.json()
                 .then((data) => {
                     var lat = data.results[0].geometry.location.lat
                     var lng = data.results[0].geometry.location.lng
-                    console.log(lng)
+                    //console.log(lng)
 
                     mapboxgl.accessToken =
                         'pk.eyJ1IjoiZ2VlYnJvd240MyIsImEiOiJjajU5MTIxYmowMzV4MnhxcDlpeGN5cHg5In0.Y1nYL-rtvgEwS3qV0YkMrQ';
@@ -22,7 +23,30 @@ el.addEventListener('submit', (event) => {
                         center: [lng, lat],
                     });
 
+    fetch(`https://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&rankby=distance&types=shoe_store&key=${key}`)
+            .then((response) => {
+                return response.json()
+                    .then((places) => {
+                        //Used to view full properties 
+                         //console.log(places)  
+                        var arrofName = [];
+                        var arrofLat = [];
+                        var arrofLng = [];
+                            for (var i = 0; i < places.results.length; i++) {
+                                var storeName = places.results[i].name
+                                arrofName.push(storeName)
+                                var storeLat = places.results[i].geometry.location.lat
+                                arrofLat.push(storeLat)
+                                var storeLng = places.results[i].geometry.location.lng
+                                arrofLng.push(storeLng)
+                                    }
+                                    console.log(arrofName, arrofLat, arrofLng)
 
+                                })
+                        })
                 })
         })
+
+
+
 })
