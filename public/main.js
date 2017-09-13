d99316332ac246d8a2a56d457880a52d57d096ec
Ellis Mwekunda <Scareb15@gmail.com>
2017-09-13 13:28:03 -0600

    added some style changes

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ var el = document.getElementById('form')
 var key = 'AIzaSyDydF9_qyXVTdAs8G7IFUJngvSu1Ic7kSY'
 var key2 = 'pk.eyJ1IjoiZ2VlYnJvd240MyIsImEiOiJjajdjNmIxeW0wMDFsMzJsajU3dzZ5MWI4In0.tGem2f5RtywDzdhUItDeCQ'
 //galvanize coordinates  lat 39.7576305, lng -105.0070158
-var galvanize = [-105.0070158, 39.7576305]
+var currentLoc = []
 el.addEventListener('submit', (event) => {
     event.preventDefault();
     var id = event.target.elements.address_input.value;
@@ -17,8 +17,9 @@ getData = (id) => {
                 .then((data) => {
                     var lat = data.results[0].geometry.location.lat
                     var lng = data.results[0].geometry.location.lng
-
-
+                    currentLoc.push(lng)
+                    currentLoc.push(lat)
+                    callMap()
                     // 20miles = 32186.9 meters
                     fetch(`https://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=32186.9&type=shoe_store&key=${key}`)
                         .then((response) => {
@@ -65,16 +66,11 @@ getData = (id) => {
                                         a.addEventListener('click', (event) => {
                                             event.preventDefault();
                                             var id = event.target
-                                            flyToStore = (a) => {
-                                                map.flyTo({
-                                                    center: [a.dataset.lng, a.dataset.lat],
-                                                    zoom: 15
-                                                });
-
-                                            }
+                                                newid.push(id)
                                             flyToStore(id)
                                             directionsAPI(id)
-
+                                        
+                                            
 
                                         })
                                     }
@@ -87,77 +83,86 @@ getData = (id) => {
         })
     return el.reset()
 }
+var newid = []
+callMap = () => {
+    mapboxgl.accessToken =
+        'pk.eyJ1IjoiZ2VlYnJvd240MyIsImEiOiJjajU5MTIxYmowMzV4MnhxcDlpeGN5cHg5In0.Y1nYL-rtvgEwS3qV0YkMrQ';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/geebrown43/cj7gp6s083y6m2stagjnq139j',
+        zoom: 10,
+        //longitude first, then latitude
+        center: [currentLoc[0], currentLoc[1]],
+    });
 
-mapboxgl.accessToken =
-    'pk.eyJ1IjoiZ2VlYnJvd240MyIsImEiOiJjajU5MTIxYmowMzV4MnhxcDlpeGN5cHg5In0.Y1nYL-rtvgEwS3qV0YkMrQ';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/geebrown43/cj7gp6s083y6m2stagjnq139j',
-    zoom: 4,
-    //longitude first, then latitude
-    center: [galvanize[0], galvanize[1]],
-});
+    flyToStore = (a) => {
+        map.flyTo({
+            center: [a.dataset.lng, a.dataset.lat],
+            zoom: 15
+        });
 
+    }
 
-directionsAPI = (a) => {
-    var end = [a.dataset.lng, a.dataset.lat]
-    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${galvanize[0]},${galvanize[1]};${a.dataset.lng},${a.dataset.lat}?steps=true&geometries=geojson&access_token=${key2}`)
-        .then((response) => {
-            return response.json()
-                .then((data) => {
-                    var route = data.routes[0].geometry;
-                    route.innerHTML = ''
-                    map.addLayer({
-                        id: 'route',
-                        type: 'line',
-                        source: {
-                            type: 'geojson',
-                            data: {
-                                type: 'Feature',
-                                geometry: route
+    directionsAPI = (a) => {
+        var end = [a.dataset.lng, a.dataset.lat]
+        fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${currentLoc[0]},${currentLoc[1]};${a.dataset.lng},${a.dataset.lat}?steps=true&geometries=geojson&access_token=${key2}`)
+            .then((response) => {
+                return response.json()
+                    .then((data) => {
+                        console.log(data)
+                        var route = data.routes[0].geometry;
+                        map.addLayer({
+                            id: 'route',
+                            type: 'line',
+                            source: {
+                                type: 'geojson',
+                                data: {
+                                    type: 'Feature',
+                                    geometry: route
+                                }
+                            },
+                            paint: {
+                                'line-width': 3
                             }
-                        },
-                        paint: {
-                            'line-width': 3
-                        }
-                    })
-                    map.addLayer({
-                        id: 'start',
-                        type: 'circle',
-                        source: {
-                            type: 'geojson',
-                            data: {
-                                type: 'Feature',
-                                geometry: {
-                                    type: 'Point',
-                                    coordinates: galvanize
+                        })
+                        map.addLayer({
+                            id: 'start',
+                            type: 'circle',
+                            source: {
+                                type: 'geojson',
+                                data: {
+                                    type: 'Feature',
+                                    geometry: {
+                                        type: 'Point',
+                                        coordinates: currentLoc
+                                    }
                                 }
                             }
-                        }
-                    });
-                    map.addLayer({
-                        id: 'end',
-                        type: 'circle',
-                        source: {
-                            type: 'geojson',
-                            data: {
-                                type: 'Feature',
-                                geometry: {
-                                    type: 'Point',
-                                    coordinates: end
+                        });
+                        map.addLayer({
+                            id: 'end',
+                            type: 'circle',
+                            source: {
+                                type: 'geojson',
+                                data: {
+                                    type: 'Feature',
+                                    geometry: {
+                                        type: 'Point',
+                                        coordinates: end
+                                    }
                                 }
                             }
-                        }
-                  
+
+                        });
+                        var instructions = document.getElementById('instructions');
+                        var steps = data.routes[0].legs[0].steps;
+                        steps.forEach(function (step) {
+                            instructions.insertAdjacentHTML('beforeend', '<p>' + step.maneuver.instruction + '</p>');
+                        })
+
                     });
-                    var instructions = document.getElementById('instructions');
-                    var steps = data.routes[0].legs[0].steps;
-                    steps.forEach(function (step) {
-                        instructions.insertAdjacentHTML('beforeend', '<p>' + step.maneuver.instruction + '</p>');
-                })
-            
-            });
-            
-        })
- 
+
+            })
+        
+    }
 }
