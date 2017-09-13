b488ad8922e32bce1a12e2a0f5ced7fb4826ad35
Ellis Mwekunda <Scareb15@gmail.com>
2017-09-13 11:41:42 -0600

    added styling and map coordinates

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ var el = document.getElementById('form')
 var key = 'AIzaSyDydF9_qyXVTdAs8G7IFUJngvSu1Ic7kSY'
 var key2 = 'pk.eyJ1IjoiZ2VlYnJvd240MyIsImEiOiJjajdjNmIxeW0wMDFsMzJsajU3dzZ5MWI4In0.tGem2f5RtywDzdhUItDeCQ'
 //galvanize coordinates  lat 39.7576305, lng -105.0070158
-var galvanize = [39.7576305, -105.0070158]
+var galvanize = [-105.0070158, 39.7576305]
 el.addEventListener('submit', (event) => {
     event.preventDefault();
     var id = event.target.elements.address_input.value;
@@ -18,24 +18,24 @@ getData = (id) => {
                     var lat = data.results[0].geometry.location.lat
                     var lng = data.results[0].geometry.location.lng
 
-                    
+
                     // 20miles = 32186.9 meters
                     fetch(`https://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lng}&radius=32186.9&type=shoe_store&key=${key}`)
                         .then((response) => {
                             return response.json()
                                 .then((places) => {
                                     //Used to view full properties 
-                                    console.log(places)
+                                    //console.log(places)
                                     var arrofName = [];
                                     var arrofLat = [];
                                     var arrofLng = [];
                                     var arrofAddress = [];
                                     var main_content = document.getElementsByClassName('main_content')[0]
                                     main_content.innerHTML = ''
-                                    
+
                                     for (var i = 0; i < places.results.length; i++) {
                                         var div = document.createElement('div')
-                                        div.className = 'store_info'                      
+                                        div.className = 'store_info'
                                         var storeName = places.results[i].name
                                         var a = document.createElement('a')
                                         a.className = 'name'
@@ -54,33 +54,30 @@ getData = (id) => {
                                         arrofLat.push(storeLat)
 
                                         var storeLng = places.results[i].geometry.location.lng
-                                        arrofLng.push(storeLng)                     
+                                        arrofLng.push(storeLng)
                                         main_content.append(div)
 
-                                        
+
                                         a.setAttribute('data-lat', storeLat)
                                         a.setAttribute('data-lng', storeLng)
-                    
-                                        
+
+
                                         a.addEventListener('click', (event) => {
                                             event.preventDefault();
                                             var id = event.target
                                             flyToStore = (a) => {
                                                 map.flyTo({
-                                                    center:[a.dataset.lng, a.dataset.lat],
-                                                    zoom : 15
+                                                    center: [a.dataset.lng, a.dataset.lat],
+                                                    zoom: 15
                                                 });
-                                                console.log(a)
-                                                
+
                                             }
-                                        //directionsAPI(a);
-                                        flyToStore(id)
-                                
+                                            flyToStore(id)
+                                            directionsAPI(id)
+
+
                                         })
-                                    
-                                        
                                     }
-                                    //console.log(arrofName, arrofLat, arrofLng)
 
 
 
@@ -92,27 +89,75 @@ getData = (id) => {
 }
 
 mapboxgl.accessToken =
-            'pk.eyJ1IjoiZ2VlYnJvd240MyIsImEiOiJjajU5MTIxYmowMzV4MnhxcDlpeGN5cHg5In0.Y1nYL-rtvgEwS3qV0YkMrQ';
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/geebrown43/cj7gp6s083y6m2stagjnq139j',
-            zoom: 4,
-            //longitude first, then latitude
-            center: [galvanize[1], galvanize[0]],
-        });
+    'pk.eyJ1IjoiZ2VlYnJvd240MyIsImEiOiJjajU5MTIxYmowMzV4MnhxcDlpeGN5cHg5In0.Y1nYL-rtvgEwS3qV0YkMrQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/geebrown43/cj7gp6s083y6m2stagjnq139j',
+    zoom: 4,
+    //longitude first, then latitude
+    center: [galvanize[0], galvanize[1]],
+});
 
 
-// directionsAPI = (a) => {
-//     fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${galvanize[1]},${galvanize[0]};${a.dataset.lng},${a.dataset.lat}?access_token=${key2}`)
-//     .then ((response) => {
-//         return response.json()
-//         .then((address) => {
-//             console.log(address)
-
+directionsAPI = (a) => {
+    var end = [a.dataset.lng, a.dataset.lat]
+    fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${galvanize[0]},${galvanize[1]};${a.dataset.lng},${a.dataset.lat}?steps=true&geometries=geojson&access_token=${key2}`)
+        .then((response) => {
+            return response.json()
+                .then((data) => {
+                    var route = data.routes[0].geometry;
+                    route.innerHTML = ''
+                    map.addLayer({
+                        id: 'route',
+                        type: 'line',
+                        source: {
+                            type: 'geojson',
+                            data: {
+                                type: 'Feature',
+                                geometry: route
+                            }
+                        },
+                        paint: {
+                            'line-width': 3
+                        }
+                    })
+                    map.addLayer({
+                        id: 'start',
+                        type: 'circle',
+                        source: {
+                            type: 'geojson',
+                            data: {
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: galvanize
+                                }
+                            }
+                        }
+                    });
+                    map.addLayer({
+                        id: 'end',
+                        type: 'circle',
+                        source: {
+                            type: 'geojson',
+                            data: {
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: end
+                                }
+                            }
+                        }
+                  
+                    });
+                    var instructions = document.getElementById('instructions');
+                    var steps = data.routes[0].legs[0].steps;
+                    steps.forEach(function (step) {
+                        instructions.insertAdjacentHTML('beforeend', '<p>' + step.maneuver.instruction + '</p>');
+                })
             
-
+            });
             
-//         })
-//     })
-// }
-
+        })
+ 
+}
